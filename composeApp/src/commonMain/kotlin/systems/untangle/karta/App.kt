dc6c3cb976b87976255b76bdbc57b82b6369ae4e
Gustavo <gustavo.c.venturini@gmail.com>
2025-03-06 23:21:31 -0300

    Tiles being dragged

--- composeApp/src/commonMain/kotlin/systems/untangle/karta/App.kt
+++ composeApp/src/commonMain/kotlin/systems/untangle/karta/App.kt
@@ -1,64 +1,69 @@
 package systems.untangle.karta
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.gestures.onDrag
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.PointerMatcher
 import androidx.compose.material.Button
+import androidx.compose.foundation.gestures.onDrag
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerButton
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
-import kotlin.io.println
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import karta.composeapp.generated.resources.Res
 import karta.composeapp.generated.resources.compose_multiplatform
+val tile_width = 256
 
 @Composable
-fun Tile(offset: Offset) {
-	println("Redrawing with ${offset}")
+fun Tile(row: Int, column: Int, xOffset: Int, yOffset: Int) {
+	val top = yOffset - column * tile_width
+	val left = xOffset + row * tile_width
 
 	Box(modifier = Modifier
+		.offset { IntOffset(left, top) }
 		.background(Color.Blue)
-		.size(150.dp)
-		.absoluteOffset {
-			IntOffset(
-				offset.x.toInt(),
-				offset.y.toInt())
-		}
-	)
+		.size(tile_width.dp))
 }
 
 @Composable
+@Preview
 @OptIn(ExperimentalFoundationApi::class)
 fun App() {
-	var offset by remember { mutableStateOf(Offset(0f, 0f)) }
+	var xOffset by remember { mutableStateOf(0f) }
+	var yOffset by remember { mutableStateOf(0f) }
 
-	Box(modifier = Modifier
-		.fillMaxWidth()
-		.fillMaxHeight()
-		.background(Color.Yellow)
-		.onDrag(matcher = PointerMatcher.mouse(PointerButton.Primary)) {
-			offset += it
-		}
+	Box(
+		Modifier
+			.fillMaxWidth()
+			.fillMaxHeight()
+			.onDrag { dragged ->
+				xOffset += dragged.x
+				yOffset += dragged.y
+			}
 	) {
-		Tile(offset)
-  }
+		Tile(-1, -1, xOffset.toInt(), yOffset.toInt())
+		Tile(+0, -1, xOffset.toInt(), yOffset.toInt())
+		Tile(+1, -1, xOffset.toInt(), yOffset.toInt())
+
+		Tile(-1, +0, xOffset.toInt(), yOffset.toInt())
+		Tile(+0, +0, xOffset.toInt(), yOffset.toInt())
+		Tile(+1, +0, xOffset.toInt(), yOffset.toInt())
+
+		Tile(-1, +1, xOffset.toInt(), yOffset.toInt())
+		Tile(+0, +1, xOffset.toInt(), yOffset.toInt())
+		Tile(+1, +1, xOffset.toInt(), yOffset.toInt())
+	}
 }
