aba673c7ad76d0656b603b56d81e5880c2cc479a
Gustavo <gustavo.c.venturini@gmail.com>
2025-03-07 17:22:52 -0300

    Movement relative to center of screen

--- composeApp/src/commonMain/kotlin/systems/untangle/karta/App.kt
+++ composeApp/src/commonMain/kotlin/systems/untangle/karta/App.kt
@@ -26,23 +26,68 @@ import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
+/*
+
+Indexed Coordinates
+
+x,y in [0,N] where N = 2^zoom
+
+.-------------------------> (x)
+| (0,0) | (1,0) | (2,0) ...
+| (0,1) | (1,1) | (2,1) ...
+| (0,2) | (1,2) | (2,2) ...
+|
+V
+(y)
+
+  0        1              2
+.---.  .---.---.  .---.---.---.---.
+|   |  |   |   |  |   |   |   |   |
+'---'  |---|---|  |---|---|---|---|
+       |   |   |  |   |   |   |   |
+			 '---'---'  |---|---|---|---|
+			            |   |   |   |   |
+									|---|---|---|---|
+									|   |   |   |   |
+									'---'---'---'---'
+
+
+C => center of screen coordinates (float)
+T => tile top left coordinates (integer)
+
+T(x,y) .------------.               -.-
+       |            |                |
+       |            |                | dY
+       |            |                |
+       |            |                |
+       '------------'               -'-
+
+       |-----------------------------| C(x,y)
+                      dX
+
+Dragging to RIGHT moves the center of screen to the LEFT and vice versa
+Dragging to TOP moves the center of screen to the BOTTOM and vice versa
+Thus => center -= drag
+
+When Cx > Tx ===> offset Tile to the LEFT (xOffset < 0) ===> xOffset = Tx - Cx
+When Cy > Ty ===> offset Tile to the TOP  (yOffset < 0) ===> yOffset = Ty - Cy
+
+*/
+
 const val tile_width = 512
 
 @Composable
 fun Tile(zoom: Int, xIndex: Int, yIndex: Int, center: Offset) {
-	val xDelta = (center.x - xIndex) * tile_width
-	val yDelta = (center.y - yIndex) * tile_width
-
-	//println("(${xIndex},${yIndex}) - (${center.x}, ${center.y}) * ${tile_width}  => xDelta = ${xDelta} / yDelta = ${yDelta}")
-	println("(${xIndex},${yIndex}) - (${center.x}, ${center.y}) => (${xDelta},${yDelta})")
+	val xOffset = (xIndex - center.x) * tile_width
+	val yOffset = (yIndex - center.y) * tile_width
 
 	Box(modifier = Modifier
-		.offset { IntOffset(xDelta.toInt(), yDelta.toInt()) }
+		.offset { IntOffset(xOffset.toInt(), yOffset.toInt()) }
 		.background(Color.Blue)
 		.size(tile_width.dp)
 	) {
 		AsyncImage(
-			model = "http://localhost:8077/${zoom}/${yIndex}/${xIndex}",
+			model = "http://localhost:8077/${zoom}/${xIndex}/${yIndex}",
 			contentDescription = ""
 		)
 	}
@@ -51,11 +96,8 @@ fun Tile(zoom: Int, xIndex: Int, yIndex: Int, center: Offset) {
 @Composable
 @OptIn(ExperimentalFoundationApi::class)
 fun App() {
-	var xOffset by remember { mutableStateOf(0f) }
-	var yOffset by remember { mutableStateOf(0f) }
-
 	var zoom by remember { mutableStateOf(14f) }
-	var center by remember { mutableStateOf(Offset(9136.5f, 6358.5f)) }
+	var center by remember { mutableStateOf(Offset(6358f, 9136f)) }
 
 	Box(
 		Modifier
@@ -63,21 +105,22 @@ fun App() {
 			.fillMaxHeight()
 			.onDrag { dragged ->
 				center = Offset(
-					center.x + (dragged.x / tile_width),
-					center.y + (dragged.y / tile_width)
+					center.x - (dragged.x / tile_width),
+					center.y - (dragged.y / tile_width)
 				)
 			}
 	) {
-		Tile(14, 9135, 6357, center)
-		Tile(14, 9136, 6357, center)
-		Tile(14, 9137, 6357, center)
+		Tile(14, 6357, 9135, center)
+		Tile(14, 6358, 9135, center)
+		Tile(14, 6359, 9135, center)
 
-		//Tile(14, 9135, 6358, center)
-		//Tile(14, 9136, 6358, center)
-		//Tile(14, 9137, 6358, center)
+		Tile(14, 6357, 9136, center)
+		Tile(14, 6358, 9136, center)
+		Tile(14, 6359, 9136, center)
 
-		//Tile(14, 9135, 6359, center)
-		//Tile(14, 9136, 6359, center)
-		//Tile(14, 9137, 6359, center)
+		Tile(14, 6357, 9137, center)
+		Tile(14, 6358, 9137, center)
+		Tile(14, 6359, 9137, center)
 	}
 }
+
