4b81bfa6170594e767405b2e362f120b9517e738
Gustavo <gustavo.c.venturini@gmail.com>
2025-03-07 15:47:50 -0300

    XY center as indicator of offset

--- composeApp/src/commonMain/kotlin/systems/untangle/karta/App.kt
+++ composeApp/src/commonMain/kotlin/systems/untangle/karta/App.kt
@@ -1,5 +1,6 @@
 package systems.untangle.karta
 
+import kotlin.io.println
 import coil3.compose.AsyncImage
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
@@ -16,6 +17,7 @@ import androidx.compose.foundation.gestures.onDrag
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.IntOffset
@@ -24,53 +26,58 @@ import androidx.compose.ui.Modifier
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import karta.composeapp.generated.resources.Res
-import karta.composeapp.generated.resources.compose_multiplatform
-val tile_width = 512
+const val tile_width = 512
 
 @Composable
-fun Tile(row: Int, column: Int, xOffset: Int, yOffset: Int) {
-	val top = yOffset + column * tile_width
-	val left = xOffset + row * tile_width
+fun Tile(zoom: Int, xIndex: Int, yIndex: Int, center: Offset) {
+	val xDelta = (center.x - xIndex) * tile_width
+	val yDelta = (center.y - yIndex) * tile_width
+
+	//println("(${xIndex},${yIndex}) - (${center.x}, ${center.y}) * ${tile_width}  => xDelta = ${xDelta} / yDelta = ${yDelta}")
+	println("(${xIndex},${yIndex}) - (${center.x}, ${center.y}) => (${xDelta},${yDelta})")
 
 	Box(modifier = Modifier
-		.offset { IntOffset(left, top) }
+		.offset { IntOffset(xDelta.toInt(), yDelta.toInt()) }
 		.background(Color.Blue)
 		.size(tile_width.dp)
 	) {
 		AsyncImage(
-			model = "http://localhost:8077/14/${6358+row}/${9136+column}",
+			model = "http://localhost:8077/${zoom}/${yIndex}/${xIndex}",
 			contentDescription = ""
 		)
 	}
 }
 
 @Composable
-@Preview
 @OptIn(ExperimentalFoundationApi::class)
 fun App() {
 	var xOffset by remember { mutableStateOf(0f) }
 	var yOffset by remember { mutableStateOf(0f) }
+
+	var zoom by remember { mutableStateOf(14f) }
+	var center by remember { mutableStateOf(Offset(9136.5f, 6358.5f)) }
 
 	Box(
 		Modifier
 			.fillMaxWidth()
 			.fillMaxHeight()
 			.onDrag { dragged ->
-				xOffset += dragged.x
-				yOffset += dragged.y
+				center = Offset(
+					center.x + (dragged.x / tile_width),
+					center.y + (dragged.y / tile_width)
+				)
 			}
 	) {
-		Tile(-1, -1, xOffset.toInt(), yOffset.toInt())
-		Tile(+0, -1, xOffset.toInt(), yOffset.toInt())
-		Tile(+1, -1, xOffset.toInt(), yOffset.toInt())
+		Tile(14, 9135, 6357, center)
+		Tile(14, 9136, 6357, center)
+		Tile(14, 9137, 6357, center)
 
-		Tile(-1, +0, xOffset.toInt(), yOffset.toInt())
-		Tile(+0, +0, xOffset.toInt(), yOffset.toInt())
-		Tile(+1, +0, xOffset.toInt(), yOffset.toInt())
+		//Tile(14, 9135, 6358, center)
+		//Tile(14, 9136, 6358, center)
+		//Tile(14, 9137, 6358, center)
 
-		Tile(-1, +1, xOffset.toInt(), yOffset.toInt())
-		Tile(+0, +1, xOffset.toInt(), yOffset.toInt())
-		Tile(+1, +1, xOffset.toInt(), yOffset.toInt())
+		//Tile(14, 9135, 6359, center)
+		//Tile(14, 9136, 6359, center)
+		//Tile(14, 9137, 6359, center)
 	}
 }
