ec2cce033f651b2e6a06c2ab4aab5f1e0e4ff855
Gustavo <gustavo.c.venturini@gmail.com>
2025-12-21 23:16:58 -0300

    Wrapping of geolocated within longitude

--- karta/src/commonMain/kotlin/systems/untangle/karta/composables/Geolocated.kt
+++ karta/src/commonMain/kotlin/systems/untangle/karta/composables/Geolocated.kt
@@ -17,26 +17,24 @@ fun Geolocated(
     content: @Composable (coordsOffset: IntOffset) -> Unit
 ) {
     val converter = LocalConverter.current
-    converter.tileRegion.topLeft.component2()
-    val coordsOffset = remember(coordinates, converter, offset) {
+    val coordsOffsets = remember(coordinates, converter, offset) {
+        val offsets = mutableListOf<IntOffset>()
         val finalOffset = offset ?: IntOffset(0, 0)
-        converter.convertToOffset(coordinates)
-            .minus(finalOffset)
+        val turns = listOf(0, -360, 360)
+
+        turns.forEach { turn ->
+            val turnCoords = coordinates.copy(longitude = coordinates.longitude - turn)
+            val offset = converter.convertToOffset(turnCoords).minus(finalOffset)
+            if (converter.insideView(coordinates, extension)) {
+                offsets.add(offset)
+            }
+        }
+
+        offsets
     }
 
-    if (converter.insideView(coordinates, extension)) {
-        content(coordsOffset.correctedPx())
-    }
-
-    val leftCoordinates = coordinates.copy(longitude = coordinates.longitude - 360.0)
-    val leftCoordsOffset = remember(leftCoordinates, converter, offset) {
-        val finalOffset = offset ?: IntOffset(0, 0)
-        converter.convertToOffset(leftCoordinates)
-            .minus(finalOffset)
-    }
-
-    if (converter.insideView(leftCoordinates, extension)) {
-        content(leftCoordsOffset.correctedPx())
+    coordsOffsets.forEach { coords ->
+        content(coords.correctedPx())
     }
 }
 
